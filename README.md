e1666f8ce45506a21ad3876bcd6ddb25ed4bbf3c
Brandon Kirschner <brandonikirschner@gmail.com>
2018-06-04 22:20:17 -0400

    Updated index.js so that pokemon.json is actually created if it does not exist for both commands. If pokemon.json is an empty array, node index.js s command will not run and instruct user to fetch the pokemon first. Added npm start utility you can run npm start instead of node index.js. By extension index.js has a multi-line console.log that renders instructions in the terminal. Readme updated accordingly

--- README.md
+++ README.md
@@ -16,10 +16,16 @@
 7. Type 2
 
 ## Instructions
-1. Clone repo
+1. Clone repo or download zip and extract folder
 2. Create a .env file in the root of the project
 3. Open the .env on a code/text editor and add `API_KEY=YOUR-ACTUAL-API-KEY-GOES-HERE`
 4. Run `npm install` in the root of the project using a CLI
-5. Run `node index.js g` to begin the fetch process. A prompt will begin, asking you for the starting pokemon number and the ending pokemon number (e.g. 1, 150). You will see "Save Completed" in the CLI's console when the pokemon.json file has been updated with the pokemon
-6. Run `node index.js s` to begin the send to HubDB process. A prompt will begin, asking for the HubDB's ID. When you enter the HubDB's ID, the script will begin sending (POST) the pokemon to the specified HubDB.
+5. Run `npm start` or `node index.js` to display the instruction screen.
+
+### Supported commands
+1 Run `node index.js g` to begin the fetch process. A prompt will begin, asking you for the starting pokemon number and the ending pokemon number (e.g. 1, 150). 
+
+You will see "Save Completed" in the CLI's console when the pokemon.json file has been updated with the pokemon. Your pokemon will be stored in a local pokemon.json file. This will be overwritten anytime you run this command.
+
+2 Run `node index.js s` to begin the send to HubDB process. A prompt will begin, asking for the HubDB's ID. When you enter the HubDB's ID, the script will begin sending (POST) the pokemon to the specified HubDB.
 
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { prompt } = require('inquirer');
 const fs = require('fs')
 const getPokemon = require('./fetchPokemon.js')
 const hubspotAPI = require('./hubspotAPI.js')
+const chalk = require('chalk');
 
 // content of index.js
 const http = require('http')
@@ -18,11 +19,18 @@ const requestHandler = (request, response) => {
 
 const server = http.createServer(requestHandler)
 
+console.log(chalk.green('Welcome to the Pokedex to HubDB Utility Script!'))
+console.log(chalk.green('Below are two commands that can be used:\n'))
+console.log("1. " + chalk.red("node index.js a") + " - used to fetch the pokemon. Start and end pokemon must be specified by number")
+console.log("2. " + chalk.red("node index.js s") + " - used to send pokemon to a hubdb table. Id of the HubDB table can be specified by number")
+console.log("\nA " + chalk.red(".env") + " file in the root of this project, with your " + chalk.red("HubSpot API Key") + ", is required")
+
+
 // https://stackoverflow.com/questions/31195391/create-a-file-if-it-doesnt-already-exist/31195572
 function createFile(filename) {
     fs.open(filename, 'r', function(err, fd) {
         if (err) {
-            fs.writeFile(filename, '', function(err) {
+            fs.writeFile(filename, '[]', function(err) {
                 if (err) {
                     console.log(err);
                 }
@@ -31,27 +39,26 @@ function createFile(filename) {
         }
     });
 }
-    	createFile('./pokemon.json')
-    	var json = require('./pokemon.json')
 
-const getQuestions = [
-  {
-    type : 'input',
-    name : 'start',
-    message : 'Choose first Pokemon by number: '
-  },
-  {
-    type : 'input',
-    name : 'end',
-    message : 'Choose last Pokemon by number: '
-  }
+createFile('./pokemon.json')
+
+const getQuestions = [{
+        type: 'input',
+        name: 'start',
+        message: 'Choose first Pokemon by number: '
+    },
+    {
+        type: 'input',
+        name: 'end',
+        message: 'Choose last Pokemon by number: '
+    }
 ];
 
 const sendQuestion = [{
-    type : 'input',
-    name : 'hubdb',
-    message : 'Choose the HubDB you want to send to by id: '
-  }]
+    type: 'input',
+    name: 'hubdb',
+    message: 'Choose the HubDB you want to send to by id: '
+}]
 
 program
     .version('0.0.1')
@@ -64,9 +71,9 @@ program
     .action(function(start, end) {
         createFile('./pokemon.json')
         prompt(getQuestions).then(function(answers) {
-        	let start = parseInt(answers['start'])
-        	let end = parseInt(answers['end'])
-        	getPokemon.recursiveFetch(start, end)
+            let start = parseInt(answers['start'])
+            let end = parseInt(answers['end'])
+            getPokemon.recursiveFetch(start, end)
         })
     });
 
@@ -75,10 +82,16 @@ program
     .alias('s')
     .description('Send Pokemon to HubDB. HubID must be specified. Run node index.js get before running this command.')
     .action(function(hubdb) {
-        prompt(sendQuestion).then(function(answers){
-        	let hubdb = parseInt(answers['hubdb'])
-        	hubspotAPI.constructPokemon(json, parseInt(hubdb))
-        })
+        createFile('./pokemon.json')
+        let cache = JSON.parse(fs.readFileSync('pokemon.json'))
+        if (cache[0]) {
+            prompt(sendQuestion).then(function(answers) {
+                let hubdb = parseInt(answers['hubdb'])
+                hubspotAPI.constructPokemon(json, parseInt(hubdb))
+            })
+        } else {
+            console.log("No Pokemon found. Run 'node index.js a' to catch some pokemon!")
+        }
     });
 
 program.parse(process.argv);
